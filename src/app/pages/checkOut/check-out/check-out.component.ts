b78be90ddfe769064f92cfbb1d9db2f8710055ff
JesusOyola <jesus.oyola@interatica.com>
2024-05-04 13:57:56 -0300

    show basic total amount

--- src/app/pages/checkOut/check-out/check-out.component.ts
+++ src/app/pages/checkOut/check-out/check-out.component.ts
@@ -9,11 +9,13 @@ import { CartService } from 'src/app/services/cart.service';
   styleUrls: ['./check-out.component.scss'],
 })
 export class CheckOutComponent implements OnInit {
-  cartListProducts: ProductAmount[] | undefined;
+  cartListProducts!: ProductAmount[];
   urlImage: string =
     'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_';
 
   cartServiceSubscription!: Subscription;
+  cartCounter!: number;
+  totalAPagar!: number;
 
   constructor(private cartService: CartService) {}
 
@@ -34,5 +36,64 @@ export class CheckOutComponent implements OnInit {
           console.log(modifiedProducts);
         },
       });
+    this.getCartCounter();
+    this.getTotalAmount();
+  }
+
+  getCartCounter() {
+    this.cartService.getItemsIntoCart().subscribe({
+      next: (data) => {
+        this.cartCounter = data;
+      },
+    });
+  }
+
+  addItem(index: number) {
+    console.log(index);
+
+    if (index !== -1) {
+      const originValue =
+        this.cartListProducts[index].precio /
+        (this.cartListProducts[index].cantidad as number);
+      this.cartListProducts[index].cantidad =
+        (this.cartListProducts[index].cantidad as number) + 1;
+
+      this.cartListProducts[index].precio =
+        originValue * (this.cartListProducts[index].cantidad as number);
+
+      this.totalAPagar = this.totalAPagar + originValue;
+    }
+
+    const updateCartCounter = this.cartCounter + 1;
+    this.cartService.setItemsIntoCart(updateCartCounter);
+  }
+
+  removeItem(index: number) {
+    console.log(index);
+    if (index !== -1) {
+      const originValue =
+        this.cartListProducts[index].precio /
+        (this.cartListProducts[index].cantidad as number);
+      this.cartListProducts[index].cantidad =
+        (this.cartListProducts[index].cantidad as number) - 1;
+
+      this.cartListProducts[index].precio =
+        originValue * (this.cartListProducts[index].cantidad as number);
+      this.totalAPagar = this.totalAPagar - originValue;
+    }
+    if (this.cartListProducts[index].cantidad === 0) {
+      this.cartListProducts.splice(index, 1);
+    }
+    const updateCartCounter = this.cartCounter - 1;
+    this.cartService.setItemsIntoCart(updateCartCounter);
+  }
+
+  getTotalAmount() {
+    let total = 0;
+
+    this.cartListProducts.forEach((product) => {
+      total = total + product.precio;
+    });
+    this.totalAPagar = total;
   }
 }
