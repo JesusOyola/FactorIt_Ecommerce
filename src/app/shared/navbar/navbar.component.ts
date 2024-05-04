841a9d9925a5ac2c10052a9800d7e5962ff704ad
JesusOyola <jesus.oyola@interatica.com>
2024-05-03 21:12:11 -0300

    show cart items

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { RouterPathNames } from 'src/app/enum/router-path-names';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  constructor(private toastr: ToastrService,  private router: Router,) {}
+export class NavbarComponent implements AfterContentChecked {
+  cartItemsAmount: number = 0;
+  constructor(
+    private toastr: ToastrService,
+    private router: Router,
+    private cartService: CartService
+  ) {}
 
-  goToCart(){
+  goToCart() {
     this.router.navigate([`/${RouterPathNames.checkout}`]);
   }
+
+  ngAfterContentChecked(): void {
+    this.cartService.getItemsIntoCart().subscribe({
+      next: (data) => {
+        this.cartItemsAmount = data;
+      },
+    });
+  }
+  
 }
